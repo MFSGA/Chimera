51586dab1611ae9b675cc0c51294e116e07aa807
MFSGA <chimera_rust@proton.me>
2025-03-01 18:16:48 +0800

    faet: modify the frontend root path

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,13 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  root: "src",
+  server: { port: 3000 },
+
   plugins: [react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-  
-  server: { port: 3000 },
 }));
