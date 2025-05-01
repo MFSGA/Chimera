ef02b9728cbba67a90a9507f4b3de2ab2b74b71c
MFSGA <chimera_rust@proton.me>
2025-05-01 19:35:36 +0800

    feat: add the react-devtools for debug

--- frontend/chimera/vite.config.ts
+++ frontend/chimera/vite.config.ts
@@ -1,17 +1,34 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 const host = process.env.TAURI_DEV_HOST;
 
+const devtools = () => {
+  return {
+    name: 'react-devtools',
+    transformIndexHtml(html: string) {
+      return html.replace(
+        /<\/head>/,
+        `<script src="http://localhost:8097"></script></head>`,
+      )
+    },
+  }
+}
+
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  root: "src",
-  server: { port: 3000 },
+export default defineConfig(async ({ command }) => {
+  const isDev = command === 'serve'
 
-  plugins: [react()],
+  const config = {
+    root: "src",
+    server: { port: 3000 },
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-}));
+    plugins: [react(), isDev && devtools(),],
+
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    //
+    // 1. prevent vite from obscuring rust errors
+    clearScreen: false,
+  } satisfies UserConfig
+  return config
+});
