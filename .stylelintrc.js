29d644cd26a29b3bf70c3e20c64992bcbdcc51d4
MFSGA <chimera_rust@proton.me>
2025-09-23 10:27:29 +0800

    feat: add stylelint support

---
+++ .stylelintrc.js
@@ -0,0 +1,95 @@
+import PostCssScss from 'postcss-scss';
+
+export default {
+  root: true,
+  defaultSeverity: 'error',
+  plugins: [
+    'stylelint-scss',
+    'stylelint-order',
+    'stylelint-declaration-block-no-ignored-properties',
+  ],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-html/html',
+    'stylelint-config-recess-order',
+  ],
+  rules: {
+    'selector-pseudo-class-no-unknown': [
+      true,
+      { ignorePseudoClasses: ['global'] },
+    ],
+    'font-family-name-quotes': null,
+    'font-family-no-missing-generic-family-keyword': null,
+    'max-nesting-depth': [
+      10,
+      {
+        ignore: ['blockless-at-rules', 'pseudo-classes'],
+      },
+    ],
+    'declaration-block-no-duplicate-properties': true,
+    'no-duplicate-selectors': true,
+    'no-descending-specificity': null,
+    'selector-class-pattern': null,
+    'value-no-vendor-prefix': [true, { ignoreValues: ['box'] }],
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'unocss',
+          'layer',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+          'config',
+          'plugin',
+          'theme',
+          'variant',
+          'custom-variant',
+          'utility',
+          'source',
+          'reference',
+        ],
+      },
+    ],
+    'at-rule-no-deprecated': [
+      true,
+      {
+        ignoreAtRules: ['apply'],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.scss', '*.scss'],
+      customSyntax: PostCssScss,
+      rules: {
+        'at-rule-no-unknown': null,
+        'import-notation': null,
+        'scss/at-rule-no-unknown': [
+          true,
+          {
+            ignoreAtRules: [
+              'tailwind',
+              'unocss',
+              'layer',
+              'apply',
+              'variants',
+              'responsive',
+              'screen',
+              'config',
+              'plugin',
+              'theme',
+              'variant',
+              'custom-variant',
+              'utility',
+              'source',
+              'reference',
+            ],
+          },
+        ],
+      },
+    },
+  ],
+};
