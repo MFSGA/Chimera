6e04e7474d583f550f0f547c84c49278f9b45b98
MFSGA <chimera_rust@proton.me>
2025-10-08 07:08:30 +0800

    feat: add lint-staged support

---
+++ .lintstagedrc.js
@@ -0,0 +1,31 @@
+export default {
+  '*.{js,cjs,.mjs,jsx}': ['prettier --write', 'eslint --cache --fix'],
+  'scripts/**/*.{ts,tsx}': [
+    'prettier --write',
+    'eslint --cache --fix',
+    () => 'tsc -p scripts/tsconfig.json --noEmit',
+  ],
+  // todo
+  'frontend/ui/**/*.{ts,tsx}': [
+    'prettier --write',
+    'eslint --cache --fix',
+    () => 'tsc -p frontend/ui/tsconfig.json --noEmit',
+  ],
+  'frontend/chimera/**/*.{ts,tsx}': [
+    'prettier --write',
+    'eslint --cache --fix',
+    () => 'tsc -p frontend/nyanpasu/tsconfig.json --noEmit',
+  ],
+  'backend/**/*.{rs,toml}': [
+    () =>
+      'cargo clippy --manifest-path=./backend/Cargo.toml --all-targets --all-features',
+    () => 'cargo fmt --manifest-path ./backend/Cargo.toml --all',
+    // () => 'cargo test --manifest-path=./backend/Cargo.toml',
+    // () => "cargo fmt --manifest-path=./backend/Cargo.toml --all",
+    // do not submit untracked files
+    // () => 'git add -u',
+  ],
+  '*.{html,sass,scss,less}': ['prettier --write', 'stylelint --fix'],
+  'package.json': ['prettier --write'],
+  '*.{md,json,jsonc,json5,yaml,yml,toml}': ['prettier --write'],
+};
