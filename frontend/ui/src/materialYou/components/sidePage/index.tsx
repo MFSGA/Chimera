de8173655db2c446388c13c818bb3a72e373d099
MFSGA <chimera_rust@proton.me>
2025-10-13 15:12:47 +0800

    feat(SidePage): add BaseErrorBoundary component and react-error-boundary package 2

--- frontend/ui/src/materialYou/components/sidePage/index.tsx
+++ frontend/ui/src/materialYou/components/sidePage/index.tsx
@@ -1,7 +1,14 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
+import { BaseErrorBoundary } from '../basePage/baseErrorBoundary';
 
-interface Props {}
+interface Props {
+  children?: ReactNode;
+}
 // todo
-export const SidePage: FC<Props> = ({}) => {
-  return <div>SidePagess</div>;
+export const SidePage: FC<Props> = ({ children }) => {
+  return (
+    <BaseErrorBoundary>
+      <div data-tauri-drag-region>{children}</div>
+    </BaseErrorBoundary>
+  );
 };
