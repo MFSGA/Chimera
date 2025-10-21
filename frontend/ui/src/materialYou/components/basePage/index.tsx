900a88529c8e2666fa3a07c0f3b3d1f0bc9a3f65
MFSGA <chimera_rust@proton.me>
2025-10-22 04:27:22 +0800

    feat(settings): optimize the BasePage component 5

--- frontend/ui/src/materialYou/components/basePage/index.tsx
+++ frontend/ui/src/materialYou/components/basePage/index.tsx
@@ -1,10 +1,22 @@
 import { FC, ReactNode, Suspense } from 'react';
+import { BaseErrorBoundary } from './baseErrorBoundary';
+import './style.scss';
+import Header from './header';
 
 interface BasePageProps {
   title?: ReactNode;
+  header?: ReactNode;
   children?: ReactNode;
 }
 
-export const BasePage: FC<BasePageProps> = ({ children }) => {
-  return <Suspense>{children}</Suspense>;
+export const BasePage: FC<BasePageProps> = ({ title, header, children }) => {
+  return (
+    <BaseErrorBoundary>
+      <div className="MDYBasePage" data-tauri-drag-region>
+        <Header title={title} header={header} />
+
+        <Suspense>{children}</Suspense>
+      </div>
+    </BaseErrorBoundary>
+  );
 };
