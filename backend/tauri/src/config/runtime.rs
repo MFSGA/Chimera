759e7e45be2aa60d117d539e46d0c34957bae3e4
MFSGA <chimera_rust@proton.me>
2025-10-23 21:04:11 +0800

    feat(patch_verge_config): patch_verge -> run_core and patch_verge completed 15

--- backend/tauri/src/config/runtime.rs
+++ backend/tauri/src/config/runtime.rs
@@ -1,5 +1,9 @@
+use serde_yaml::Mapping;
+
 #[derive(Default, Debug, Clone)]
-pub struct IRuntime {}
+pub struct IRuntime {
+    pub config: Option<Mapping>,
+}
 
 impl IRuntime {
     pub fn new() -> Self {
