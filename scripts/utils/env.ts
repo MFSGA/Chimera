995ae164c8e3befb0a1ad7e529fd33dbeb97fead
MFSGA <chimera_rust@proton.me>
2025-10-31 16:47:24 +0800

    feat(check_update): prepare for the command 1

--- scripts/utils/env.ts
+++ scripts/utils/env.ts
@@ -3,3 +3,10 @@ import path from 'path';
 export const cwd = process.cwd();
 export const TAURI_APP_DIR = path.join(cwd, 'backend/tauri');
 export const TEMP_DIR = path.join(cwd, 'node_modules/.verge');
+
+// used for git-info
+export const TAURI_APP_TEMP_DIR = path.join(TAURI_APP_DIR, 'tmp');
+export const GIT_SUMMARY_INFO_PATH = path.join(
+  TAURI_APP_TEMP_DIR,
+  'git-info.json',
+);
