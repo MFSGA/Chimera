21a708756b6a1130c836eff2812ea84d22642106
MFSGA <chimera_rust@proton.me>
2025-11-02 14:29:56 +0800

    feat(ProxyPage): complete the GroupList component for frontend 5

--- frontend/chimera/src/pages/proxies.tsx
+++ frontend/chimera/src/pages/proxies.tsx
@@ -1,7 +1,9 @@
 import { useClashProxies, useProxyMode } from '@chimera/interface';
 import { SidePage } from '@chimera/ui';
 import { createFileRoute } from '@tanstack/react-router';
+import { RefObject, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
+import { GroupList } from '@/components/proxies/group-list';
 
 export const Route = createFileRoute('/proxies')({
   component: ProxyPage,
@@ -16,23 +18,34 @@ function ProxyPage() {
 
   const hasProxies = Boolean(data?.groups.length);
 
+  const leftViewportRef = useRef<HTMLDivElement>(null);
+
+  const rightViewportRef = useRef<HTMLDivElement>(null);
+
   console.log(data);
 
   return (
-    <SidePage>
+    <SidePage
+      title={t('Proxy Groups')}
+      leftViewportRef={leftViewportRef}
+      rightViewportRef={rightViewportRef}
+      side={
+        hasProxies &&
+        proxyMode.rule && (
+          <GroupList scrollRef={leftViewportRef as RefObject<HTMLElement>} />
+        )
+      }
+    >
       {!proxyMode.direct ? (
         hasProxies ? (
           <>
-            <div>
-              {/* <ContentDisplay className="absolute" message={t('Direct Mode')} /> */}
-              hasProxies
-              {/* <NodeList
+            <div>hasProxies</div>
+            {/* <NodeList
               ref={nodeListRef}
               scrollRef={rightViewportRef as RefObject<HTMLElement>}
-            />
+            /> */}
 
-            <DelayButton onClick={handleDelayClick} /> */}
-            </div>
+            {/*<DelayButton onClick={handleDelayClick} /> */}
           </>
         ) : (
           <div>
