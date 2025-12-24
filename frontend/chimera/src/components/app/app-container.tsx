fd516fb2f51b1d4b6646486c75a8a72e216e6c17
MFSGA <chimera_rust@proton.me>
2025-12-24 13:42:48 +0800

    feat(window): support right size fow window

--- frontend/chimera/src/components/app/app-container.tsx
+++ frontend/chimera/src/components/app/app-container.tsx
@@ -1,7 +1,18 @@
+import { alpha, getSystem } from '@chimera/ui';
+import { Box } from '@mui/material';
 import Paper from '@mui/material/Paper';
-import { ReactNode, useEffect, useRef } from 'react';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow';
+import { ReactNode } from 'react';
+import { LayoutControl } from '../layout/layout-control';
 import styles from './app-container.module.scss';
 import { DrawerContent } from './drawer-content';
+
+// todo: add the css style file
+
+const appWindow = getCurrentWebviewWindow();
+
+const OS = getSystem();
 
 export const AppContainer = ({
   children,
@@ -10,6 +21,11 @@ export const AppContainer = ({
   children?: ReactNode;
   isDrawer?: boolean;
 }) => {
+  const { data: isMaximized } = useSuspenseQuery({
+    queryKey: ['isMaximized'],
+    queryFn: () => appWindow.isMaximized(),
+  });
+
   return (
     <Paper
       square
@@ -30,7 +46,28 @@ export const AppContainer = ({
           <DrawerContent />
         </div>
       )}
-      <div>{children}</div>
+
+      <div className={styles.container}>
+        {OS === 'windows' && (
+          <LayoutControl className="!z-top fixed top-2 right-4" />
+        )}
+        {/* TODO: add a framer motion animation to toggle the maximized state */}
+        {OS === 'macos' && !isMaximized && (
+          <Box
+            className="z-top fixed top-1.5 left-3 h-7 w-[4.5rem] rounded-full"
+            sx={(theme) => ({
+              backgroundColor: alpha(theme.vars.palette.primary.main, 0.1),
+            })}
+          />
+        )}
+
+        <div
+          className={OS === 'macos' ? 'h-[2.75rem]' : 'h-9'}
+          data-tauri-drag-region
+        />
+
+        {children}
+      </div>
     </Paper>
   );
 };
