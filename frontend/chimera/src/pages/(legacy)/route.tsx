c03f32f5f2e8c8166fe98389d3060eb7a860350a
MFSGA <chimera_rust@proton.me>
2026-04-07 12:07:27 +0800

    feat(layout): refactor root component

--- frontend/chimera/src/pages/(legacy)/route.tsx
+++ frontend/chimera/src/pages/(legacy)/route.tsx
@@ -1,9 +1,93 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { useSettings } from '@chimera/interface';
+import { cn } from '@chimera/ui';
+import { CssBaseline } from '@mui/material';
+import { StyledEngineProvider } from '@mui/material/styles';
+import { createFileRoute, useLocation } from '@tanstack/react-router';
+import { useAtom, useSetAtom } from 'jotai';
+import { PropsWithChildren, useEffect } from 'react';
+import { SWRConfig } from 'swr';
+import { AppContainer } from '@/components/app/app-container';
+import LocalesProvider from '@/components/app/locales-provider';
+import MutationProvider from '@/components/layout/mutation-provider';
+import NoticeProvider from '@/components/layout/notice-provider';
+import PageTransition from '@/components/layout/page-transition';
+import SchemeProvider from '@/components/layout/scheme-provider';
+import { ThemeModeProvider } from '@/components/layout/use-custom-theme';
+import UpdaterDialog from '@/components/updater/updater-dialog-wrapper';
+import { UpdaterProvider } from '@/hooks/use-updater';
+import { FileRouteTypes } from '@/routeTree.gen';
+import { atomIsDrawer, memorizedRoutePathAtom } from '@/store';
 
 export const Route = createFileRoute('/(legacy)')({
-  component: LegacyLayout,
+  component: Layout,
 });
 
-function LegacyLayout() {
-  return <Outlet />;
+const QueryLoaderProvider = ({ children }: PropsWithChildren) => {
+  const {
+    query: { isLoading },
+  } = useSettings();
+
+  return isLoading ? null : children;
+};
+
+function Layout() {
+  const [isDrawer, setIsDrawer] = useAtom(atomIsDrawer);
+  const setMemorizedPath = useSetAtom(memorizedRoutePathAtom);
+  const pathname = useLocation({
+    select: (location) => location.pathname,
+  });
+
+  useEffect(() => {
+    if (pathname !== '/') {
+      setMemorizedPath(pathname as FileRouteTypes['to']);
+    }
+  }, [pathname, setMemorizedPath]);
+
+  useEffect(() => {
+    const syncDrawerState = () => {
+      setIsDrawer(window.innerWidth < 900);
+    };
+
+    syncDrawerState();
+    window.addEventListener('resize', syncDrawerState);
+
+    return () => {
+      window.removeEventListener('resize', syncDrawerState);
+    };
+  }, [setIsDrawer]);
+
+  return (
+    <SWRConfig
+      value={{
+        errorRetryCount: 5,
+        revalidateOnMount: true,
+        revalidateOnFocus: true,
+        refreshInterval: 5000,
+      }}
+    >
+      <QueryLoaderProvider>
+        <StyledEngineProvider injectFirst>
+          <ThemeModeProvider>
+            <CssBaseline />
+            <LocalesProvider />
+            <MutationProvider>
+              <NoticeProvider />
+              <SchemeProvider />
+              <UpdaterDialog />
+              <UpdaterProvider />
+
+              <AppContainer isDrawer={isDrawer}>
+                <PageTransition
+                  className={cn(
+                    'absolute inset-4 top-10',
+                    !isDrawer && 'left-0',
+                  )}
+                />
+              </AppContainer>
+            </MutationProvider>
+          </ThemeModeProvider>
+        </StyledEngineProvider>
+      </QueryLoaderProvider>
+    </SWRConfig>
+  );
 }
--- frontend/chimera/src/pages/__root.tsx
+++ frontend/chimera/src/pages/__root.tsx
@@ -1,32 +1,16 @@
-import { RootProvider, useSettings } from '@chimera/interface';
-import { CssBaseline } from '@mui/material';
-import { StyledEngineProvider } from '@mui/material/styles';
-import { createRootRoute, useLocation } from '@tanstack/react-router';
+import { RootProvider } from '@chimera/interface';
+import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { emit } from '@tauri-apps/api/event';
 import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow';
 import { useMount } from 'ahooks';
-import { lazy, PropsWithChildren, useEffect } from 'react';
-import { SWRConfig } from 'swr';
-import { AppContainer } from '@/components/app/app-container';
-import LocalesProvider from '@/components/app/locales-provider';
-import MutationProvider from '@/components/layout/mutation-provider';
-import NoticeProvider from '@/components/layout/notice-provider';
-import SchemeProvider from '@/components/layout/scheme-provider';
-import { ThemeModeProvider } from '@/components/layout/use-custom-theme';
-import UpdaterDialog from '@/components/updater/updater-dialog-wrapper';
+import { lazy } from 'react';
 import { useNyanpasuStorageSubscribers } from '@/hooks/use-store';
-import { UpdaterProvider } from '@/hooks/use-updater';
-import { atomIsDrawer, memorizedRoutePathAtom } from '@/store';
 import 'dayjs/locale/ru';
 import 'dayjs/locale/zh-cn';
 import 'dayjs/locale/zh-tw';
-import { cn } from '@chimera/ui';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import { useAtom, useSetAtom } from 'jotai';
-import PageTransition from '@/components/layout/page-transition';
-import { FileRouteTypes } from '@/routeTree.gen';
 
 dayjs.extend(relativeTime);
 dayjs.extend(customParseFormat);
@@ -48,41 +32,8 @@ export const Route = createRootRoute({
   // pendingComponent: Pending,
 });
 
-const QueryLoaderProvider = ({ children }: PropsWithChildren) => {
-  const {
-    query: { isLoading },
-  } = useSettings();
-
-  return isLoading ? null : children;
-};
-
 export default function App() {
   useNyanpasuStorageSubscribers();
-
-  const [isDrawer, setIsDrawer] = useAtom(atomIsDrawer);
-  const setMemorizedPath = useSetAtom(memorizedRoutePathAtom);
-  const pathname = useLocation({
-    select: (location) => location.pathname,
-  });
-
-  useEffect(() => {
-    if (pathname !== '/') {
-      setMemorizedPath(pathname as FileRouteTypes['to']);
-    }
-  }, [pathname, setMemorizedPath]);
-
-  useEffect(() => {
-    const syncDrawerState = () => {
-      setIsDrawer(window.innerWidth < 900);
-    };
-
-    syncDrawerState();
-    window.addEventListener('resize', syncDrawerState);
-
-    return () => {
-      window.removeEventListener('resize', syncDrawerState);
-    };
-  }, [setIsDrawer]);
 
   useMount(() => {
     const appWindow = getCurrentWebviewWindow();
@@ -103,40 +54,8 @@ export default function App() {
 
   return (
     <RootProvider>
-      <SWRConfig
-        value={{
-          errorRetryCount: 5,
-          revalidateOnMount: true,
-          revalidateOnFocus: true,
-          refreshInterval: 5000,
-        }}
-      >
-        <QueryLoaderProvider>
-          <StyledEngineProvider injectFirst>
-            <ThemeModeProvider>
-              <CssBaseline />
-              <LocalesProvider />
-              <MutationProvider>
-                <NoticeProvider />
-                <SchemeProvider />
-                <UpdaterDialog />
-                <UpdaterProvider />
-
-                <AppContainer isDrawer={isDrawer}>
-                  <PageTransition
-                    className={cn(
-                      'absolute inset-4 top-10',
-                      !isDrawer && 'left-0',
-                    )}
-                  />
-
-                  <TanStackRouterDevtools />
-                </AppContainer>
-              </MutationProvider>
-            </ThemeModeProvider>
-          </StyledEngineProvider>
-        </QueryLoaderProvider>
-      </SWRConfig>
+      <Outlet />
+      <TanStackRouterDevtools />
     </RootProvider>
   );
 }
