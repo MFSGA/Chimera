e19d482097ac4687fd63a876c9b2fecac3d50c61
MFSGA <chimera_rust@proton.me>
2026-04-20 18:32:22 +0800

    feat: support auto-launch

--- frontend/chimera/src/components/setting/setting-system-behavior.tsx
+++ frontend/chimera/src/components/setting/setting-system-behavior.tsx
@@ -1,104 +1,48 @@
-import { useSetting, type BreakWhenProxyChange } from '@chimera/interface';
-import { BaseCard, MenuItem, SwitchItem } from '@chimera/ui';
-import { List } from '@mui/material';
-import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow';
+import { useSetting } from '@chimera/interface';
+import { BaseCard } from '@chimera/ui';
+import { Grid } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { PaperSwitchButton } from './modules/system-proxy';
 
-const appWindow = getCurrentWebviewWindow();
-
-const AlwaysOnTopButton = () => {
+const AutoStartButton = () => {
   const { t } = useTranslation();
-  const alwaysOnTop = useSetting('always_on_top');
-
-  const handleToggle = async () => {
-    const nextValue = !alwaysOnTop.value;
-    await alwaysOnTop.upsert(nextValue);
-    await appWindow.setAlwaysOnTop(nextValue);
-  };
+  const autoLaunch = useSetting('enable_auto_launch');
 
   return (
-    <SwitchItem
-      label={t('Always On Top')}
-      checked={Boolean(alwaysOnTop.value)}
-      onChange={handleToggle}
+    <PaperSwitchButton
+      label={t('Auto Start')}
+      checked={autoLaunch.value || false}
+      onClick={() => autoLaunch.upsert(!autoLaunch.value)}
     />
   );
 };
 
-const AutoCheckUpdateButton = () => {
+const SilentStartButton = () => {
   const { t } = useTranslation();
-  const autoCheckUpdate = useSetting('enable_auto_check_update');
+  const silentStart = useSetting('enable_silent_start');
 
   return (
-    <SwitchItem
-      label={t('Auto Check Updates')}
-      checked={autoCheckUpdate.value ?? true}
-      onChange={() => autoCheckUpdate.upsert(!(autoCheckUpdate.value ?? true))}
-    />
-  );
-};
-
-const BreakWhenProfileChangeButton = () => {
-  const { t } = useTranslation();
-  const breakWhenProfileChange = useSetting('break_when_profile_change');
-
-  return (
-    <SwitchItem
-      label={t('Disconnect on Profile Change')}
-      checked={Boolean(breakWhenProfileChange.value)}
-      onChange={() =>
-        breakWhenProfileChange.upsert(!breakWhenProfileChange.value)
-      }
-    />
-  );
-};
-
-const BreakWhenModeChangeButton = () => {
-  const { t } = useTranslation();
-  const breakWhenModeChange = useSetting('break_when_mode_change');
-
-  return (
-    <SwitchItem
-      label={t('Disconnect on Mode Change')}
-      checked={Boolean(breakWhenModeChange.value)}
-      onChange={() => breakWhenModeChange.upsert(!breakWhenModeChange.value)}
-    />
-  );
-};
-
-const BreakWhenProxyChangeItem = () => {
-  const { t } = useTranslation();
-  const breakWhenProxyChange = useSetting('break_when_proxy_change');
-
-  return (
-    <MenuItem
-      label={t('Disconnect on Proxy Change')}
-      selectSx={{ width: 180 }}
-      selected={breakWhenProxyChange.value ?? 'none'}
-      options={{
-        none: t('Keep Connections'),
-        chain: t('Disconnect Active Chain'),
-        all: t('Disconnect All Connections'),
-      }}
-      onSelected={(value) =>
-        breakWhenProxyChange.upsert(value as BreakWhenProxyChange)
-      }
+    <PaperSwitchButton
+      label={t('Silent Start')}
+      checked={silentStart.value || false}
+      onClick={() => silentStart.upsert(!silentStart.value)}
     />
   );
 };
 
 export const SettingSystemBehavior = () => {
   const { t } = useTranslation();
-
   return (
-    <BaseCard label={t('System Behavior')}>
-      <List disablePadding>
-        <AlwaysOnTopButton />
-        <AutoCheckUpdateButton />
-        <BreakWhenProfileChangeButton />
-        <BreakWhenModeChangeButton />
-        <BreakWhenProxyChangeItem />
-      </List>
+    <BaseCard label={t('Initiating Behavior')}>
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 6 }}>
+          <AutoStartButton />
+        </Grid>
+
+        <Grid size={{ xs: 6 }}>
+          <SilentStartButton />
+        </Grid>
+      </Grid>
     </BaseCard>
   );
 };
