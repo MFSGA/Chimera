a049f22a1fa6b600af7310650167ac83a907e328
MFSGA <chimera_rust@proton.me>
2026-05-08 12:19:59 +0800

    feat(collect_envs): support feedback icon

--- frontend/chimera/src/utils/index.ts
+++ frontend/chimera/src/utils/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { type EnvInfo } from '@chimera/interface';
 import { includes, isArray, isObject, isString, some } from 'lodash-es';
 
 export function formatError(err: unknown): string {
@@ -18,3 +19,28 @@ export const containsSearchTerm = (obj: any, term: string): boolean => {
 
   return false;
 };
+
+export function formatEnvInfos(envs: EnvInfo) {
+  let result = '----------- System -----------\n';
+  result += `OS: ${envs.os}\n`;
+  result += `Arch: ${envs.arch}\n`;
+  result += `----------- Device -----------\n`;
+  for (const cpu of envs.device.cpu) {
+    result += `CPU: ${cpu}\n`;
+  }
+  result += `Memory: ${envs.device.memory}\n`;
+  result += `----------- Core -----------\n`;
+  for (const key in envs.core) {
+    result += `${key}: \`${envs.core[key]}\`\n`;
+  }
+  result += `----------- Build Info -----------\n`;
+  for (const k of Object.keys(envs.build_info)) {
+    const key = k
+      .split('_')
+      .map((v: string) => v.charAt(0).toUpperCase() + v.slice(1))
+      .join(' ');
+    result += `${key}: ${envs.build_info[k as keyof EnvInfo['build_info']]}\n`;
+  }
+
+  return result;
+}
