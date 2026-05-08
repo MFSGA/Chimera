770c989499400dd156642f9aaa24b23476893089
MFSGA <chimera_rust@proton.me>
2026-05-08 12:19:59 +0800

    feat(collect_envs): support feedback icon

--- frontend/chimera/src/pages/(legacy)/settings.tsx
+++ frontend/chimera/src/pages/(legacy)/settings.tsx
@@ -1,11 +1,12 @@
-import { openThat } from '@chimera/interface';
+import { commands, openThat } from '@chimera/interface';
 import { BasePage } from '@chimera/ui';
-import { GitHub } from '@mui/icons-material';
+import { Feedback, GitHub } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
 import { createFileRoute } from '@tanstack/react-router';
 import { useLockFn } from 'ahooks';
 import { lazy } from 'react';
 import { useTranslation } from 'react-i18next';
+import { formatEnvInfos } from '@/utils';
 
 export const Route = createFileRoute('/(legacy)/settings')({
   component: SettingPage,
@@ -28,11 +29,40 @@ function SettingPage() {
     );
   };
 
+  const FeedbackIcon = () => {
+    const toFeedback = useLockFn(async () => {
+      const envs = await commands.collectEnvs();
+
+      if (envs.status !== 'ok') {
+        return;
+      }
+
+      const formattedEnv = encodeURIComponent(
+        formatEnvInfos(envs.data)
+          .split('\n')
+          .map((v) => `> ${v}`)
+          .join('\n'),
+      );
+
+      return openThat(
+        'https://github.com/MFSGA/Chimera/issues/new?assignees=&labels=T%3A+Bug%2CS%3A+Untriaged&projects=&template=bug_report.yaml&env_infos=' +
+          formattedEnv,
+      );
+    });
+
+    return (
+      <IconButton color="inherit" title={t('Feedback')} onClick={toFeedback}>
+        <Feedback fontSize="inherit" />
+      </IconButton>
+    );
+  };
+
   return (
     <BasePage
       title={t('Settings')}
       header={
         <div className="flex gap-1">
+          <FeedbackIcon />
           <GithubIcon />
         </div>
       }
