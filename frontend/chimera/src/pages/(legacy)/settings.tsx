e9fa729aab3c575e283efa6231409bbfb579bc96
MFSGA <chimera_rust@proton.me>
2026-05-08 11:22:48 +0800

    feat(Setting): add a new GithubIcon

--- frontend/chimera/src/pages/(legacy)/settings.tsx
+++ frontend/chimera/src/pages/(legacy)/settings.tsx
@@ -1,5 +1,9 @@
+import { openThat } from '@chimera/interface';
 import { BasePage } from '@chimera/ui';
+import { GitHub } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
 import { createFileRoute } from '@tanstack/react-router';
+import { useLockFn } from 'ahooks';
 import { lazy } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -12,8 +16,27 @@ function SettingPage() {
   // vital
   const Component = lazy(() => import('@/components/setting/setting-page'));
 
+  const GithubIcon = () => {
+    const toGithubRepo = useLockFn(() => {
+      return openThat('https://github.com/MFSGA/Chimera');
+    });
+
+    return (
+      <IconButton color="inherit" title="@MFSGA/Chimera" onClick={toGithubRepo}>
+        <GitHub fontSize="inherit" />
+      </IconButton>
+    );
+  };
+
   return (
-    <BasePage title={t('Settings')}>
+    <BasePage
+      title={t('Settings')}
+      header={
+        <div className="flex gap-1">
+          <GithubIcon />
+        </div>
+      }
+    >
       <Component />
     </BasePage>
   );
