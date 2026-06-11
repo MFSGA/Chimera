0aa41972663f6a88a869110c3c3cb6a9d37bf1a2
MFSGA <chimera_rust@proton.me>
2026-06-11 19:03:08 +0800

    feat(providers): prepare for rule providers

--- frontend/chimera/src/pages/(main)/main/providers/index.tsx
+++ frontend/chimera/src/pages/(main)/main/providers/index.tsx
@@ -1,9 +1,62 @@
+import { cn } from '@chimera/utils';
 import { createFileRoute, Link } from '@tanstack/react-router';
+import { PropsWithChildren } from 'react';
+import { Button } from '@/components/ui/button';
+import { m } from '@/paraglide/messages';
 
 export const Route = createFileRoute('/(main)/main/providers/')({
-  component: ProvidersPage,
+  component: RouteComponent,
 });
 
-function ProvidersPage() {
-  return <div className="flex flex-col gap-4 p-4">ProvidersPage</div>;
+const Group = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="flex flex-col gap-1" data-slot="providers-group">
+      {children}
+    </div>
+  );
+};
+
+const GroupTitle = ({ children }: PropsWithChildren) => {
+  return (
+    <div
+      className={cn(
+        'sticky top-0 z-10 pl-1 text-lg font-semibold',
+        'text-secondary bg-mixed-background flex h-16 items-center justify-between',
+      )}
+      data-slot="providers-group-title"
+    >
+      {children}
+    </div>
+  );
+};
+/* 
+const GroupContent = ({ children }: PropsWithChildren) => {
+  return (
+    <div
+      className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4"
+      data-slot="providers-group-content"
+    >
+      {children}
+    </div>
+  );
+}; */
+
+function RouteComponent() {
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <Group>
+        <GroupTitle>
+          <span>{m.providers_rules_title()}</span>
+
+          <Button
+            icon
+            /* onClick={handleUpdateRules}
+            loading={rulesBlockTask.isPending} */
+          >
+            <div>hello tood</div>
+          </Button>
+        </GroupTitle>
+      </Group>
+    </div>
+  );
 }
