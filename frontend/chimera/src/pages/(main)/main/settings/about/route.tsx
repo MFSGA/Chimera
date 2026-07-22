10dea2323ea8a05e91ca39a2f3833a94587ade97
MFSGA <chimera_rust@proton.me>
2026-07-22 17:52:26 +0800

    feat(frontend): add ref update action to about route

--- frontend/chimera/src/pages/(main)/main/settings/about/route.tsx
+++ frontend/chimera/src/pages/(main)/main/settings/about/route.tsx
@@ -1,10 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router';
+import z from 'zod';
 import SettingChimeraVersion from '@/components/setting/setting-chimera-version';
 import * as m from '@/paraglide/messages';
 import { SettingsTitle } from '../_modules/settings-title';
 
+export enum Action {
+  NEED_UPDATE = 'need-update',
+}
+
 export const Route = createFileRoute('/(main)/main/settings/about')({
   component: RouteComponent,
+  validateSearch: z.object({
+    action: z.enum(Action).optional().nullable(),
+  }),
 });
 
 function RouteComponent() {
