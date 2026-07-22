bc80fc9d1b3c9b3c398da2b92cf568c750ccd8e7
MFSGA <chimera_rust@proton.me>
2026-07-22 17:53:20 +0800

    feat(frontend): show ref update action in header

--- frontend/chimera/src/pages/(main)/_modules/header-menu.tsx
+++ frontend/chimera/src/pages/(main)/_modules/header-menu.tsx
@@ -1,6 +1,9 @@
 import { cn } from '@chimera/utils';
+import { Link } from '@tanstack/react-router';
 import type { ComponentProps } from 'react';
+import { useChimeraUpdate } from '@/components/providers/chimera-update-provider';
 import { Button, type ButtonProps } from '@/components/ui/button';
+import { Action as AboutAction } from '@/pages/(main)/main/settings/about/route';
 import * as m from '@/paraglide/messages';
 import HeaderFileAction from './header-file-action';
 import HeaderHelpAction from './header-help-action';
@@ -16,6 +19,38 @@ const MenuButton = ({ className, ...props }: ButtonProps) => (
     {...props}
   />
 );
+
+const UpdateButton = () => {
+  const { hasNewVersion } = useChimeraUpdate();
+
+  if (!hasNewVersion) {
+    return null;
+  }
+
+  return (
+    <div className="relative">
+      <MenuButton className="flex items-center justify-center" asChild>
+        <Link
+          to="/main/settings/about"
+          search={{
+            action: AboutAction.NEED_UPDATE,
+          }}
+        >
+          {m.header_new_version()}
+        </Link>
+      </MenuButton>
+
+      <span className="bg-on-error-container absolute top-1 right-0.5 size-1.5 rounded-full" />
+
+      <span
+        className={cn(
+          'absolute top-1 right-0.5 size-1.5 animate-ping rounded-full',
+          'bg-on-error-container',
+        )}
+      />
+    </div>
+  );
+};
 
 export default function HeaderMenu({
   className,
@@ -38,6 +73,8 @@ export default function HeaderMenu({
       <HeaderHelpAction>
         <MenuButton>{m.header_help_action_title()}</MenuButton>
       </HeaderHelpAction>
+
+      <UpdateButton />
     </div>
   );
 }
