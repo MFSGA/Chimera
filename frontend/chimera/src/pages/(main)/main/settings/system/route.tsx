b5acda0be76e04d32d35cc1e55b77636a038f411
MFSGA <chimera_rust@proton.me>
2026-08-08 20:52:11 -0700

    refactor(settings): align system settings

--- frontend/chimera/src/pages/(main)/main/settings/system/route.tsx
+++ frontend/chimera/src/pages/(main)/main/settings/system/route.tsx
@@ -1,10 +1,10 @@
 import { useIsAppImage } from '@chimera/interface';
 import { createFileRoute } from '@tanstack/react-router';
-import SettingSystemBehavior from '@/components/setting/setting-system-behavior';
-import SettingSystemProxy from '@/components/setting/setting-system-proxy';
-import SettingSystemService from '@/components/setting/setting-system-service';
 import * as m from '@/paraglide/messages';
 import { SettingsTitle } from '../_modules/settings-title';
+import SystemBehavior from './_modules/system-behavior';
+import SystemProxy from './_modules/system-proxy';
+import SystemService from './_modules/system-service';
 
 export const Route = createFileRoute('/(main)/main/settings/system')({
   component: RouteComponent,
@@ -17,10 +17,10 @@ function RouteComponent() {
     <>
       <SettingsTitle>{m.settings_label_system()}</SettingsTitle>
 
-      <div className="grid gap-4 px-4 pb-4 xl:grid-cols-2">
-        <SettingSystemProxy />
-        <SettingSystemBehavior />
-        {!isAppImage.data && <SettingSystemService />}
+      <div className="space-y-4 px-4 pb-4">
+        <SystemProxy />
+        {!isAppImage.data && <SystemService />}
+        <SystemBehavior />
       </div>
     </>
   );
