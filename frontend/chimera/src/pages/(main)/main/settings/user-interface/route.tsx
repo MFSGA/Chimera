f5d6d698ec7e598cb52fc79bf78914a942d07913
MFSGA <chimera_rust@proton.me>
2026-08-08 20:52:28 -0700

    refactor(settings): align user interface settings

--- frontend/chimera/src/pages/(main)/main/settings/user-interface/route.tsx
+++ frontend/chimera/src/pages/(main)/main/settings/user-interface/route.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
-import SettingChimeraUI from '@/components/setting/setting-chimera-ui';
 import * as m from '@/paraglide/messages';
 import { SettingsTitle } from '../_modules/settings-title';
+import UserInterfaceSettings from './_modules/user-interface-settings';
 
 export const Route = createFileRoute('/(main)/main/settings/user-interface')({
   component: RouteComponent,
@@ -12,8 +12,8 @@ function RouteComponent() {
     <>
       <SettingsTitle>{m.settings_label_user_interface()}</SettingsTitle>
 
-      <div className="grid gap-4 px-4 pb-4 xl:grid-cols-2">
-        <SettingChimeraUI />
+      <div className="space-y-4 px-4 pb-4">
+        <UserInterfaceSettings />
       </div>
     </>
   );
