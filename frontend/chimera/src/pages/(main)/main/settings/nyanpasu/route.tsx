f6a7685e78321430008a563a87b950acb6157bbb
MFSGA <chimera_rust@proton.me>
2026-08-08 20:52:20 -0700

    refactor(settings): align nyanpasu settings

--- frontend/chimera/src/pages/(main)/main/settings/nyanpasu/route.tsx
+++ frontend/chimera/src/pages/(main)/main/settings/nyanpasu/route.tsx
@@ -1,8 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
-import SettingChimeraMisc from '@/components/setting/setting-chimera-misc';
-import SettingChimeraTasks from '@/components/setting/setting-chimera-tasks';
 import * as m from '@/paraglide/messages';
 import { SettingsTitle } from '../_modules/settings-title';
+import ChimeraSettings from './_modules/chimera-settings';
 
 export const Route = createFileRoute('/(main)/main/settings/nyanpasu')({
   component: RouteComponent,
@@ -12,10 +11,8 @@ function RouteComponent() {
   return (
     <>
       <SettingsTitle>{m.settings_label_nyanpasu()}</SettingsTitle>
-
-      <div className="grid gap-4 px-4 pb-4 xl:grid-cols-2">
-        <SettingChimeraMisc />
-        <SettingChimeraTasks />
+      <div className="space-y-4 px-4 pb-4">
+        <ChimeraSettings />
       </div>
     </>
   );
