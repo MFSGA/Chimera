f67cf1d10059a668014efc6e4792981016b14486
MFSGA <chimera_rust@proton.me>
2026-08-08 20:57:24 -0700

    refactor(settings): add system tools

--- frontend/chimera/src/pages/(main)/main/settings/system/route.tsx
+++ frontend/chimera/src/pages/(main)/main/settings/system/route.tsx
@@ -1,10 +1,14 @@
 import { useIsAppImage } from '@chimera/interface';
+import { getSystem } from '@chimera/ui';
 import { createFileRoute } from '@tanstack/react-router';
 import * as m from '@/paraglide/messages';
 import { SettingsTitle } from '../_modules/settings-title';
 import SystemBehavior from './_modules/system-behavior';
 import SystemProxy from './_modules/system-proxy';
 import SystemService from './_modules/system-service';
+import SystemTools from './_modules/system-tools';
+
+const OS = getSystem();
 
 export const Route = createFileRoute('/(main)/main/settings/system')({
   component: RouteComponent,
@@ -21,6 +25,7 @@ function RouteComponent() {
         <SystemProxy />
         {!isAppImage.data && <SystemService />}
         <SystemBehavior />
+        {OS === 'windows' && <SystemTools />}
       </div>
     </>
   );
